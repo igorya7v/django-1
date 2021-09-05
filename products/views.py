7449c989c049a30f434edaf0c73cc56fe829f90f
Igor Yakubov <igor.yakubov@telus.com>
2021-09-04 17:04:08 -0700

    add products and new-products views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# /products -> index
+def index(request):
+    return HttpResponse('Products Page')
+
+
+def new(request):
+    return HttpResponse('The New Products Page')
