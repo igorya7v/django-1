5fed994d21e78b036ac48cff902095cd4faf5aa0
Igor Yakubov <igor.yakubov@telus.com>
2021-09-05 15:42:09 -0700

    load the products in the products page

--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 # /products -> index
 def index(request):
-    return HttpResponse('Products Page')
+    # Product.objects.filter(...)
+    # Product.objects.get(...)
+    # Product.objects.save(...)
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def new(request):
